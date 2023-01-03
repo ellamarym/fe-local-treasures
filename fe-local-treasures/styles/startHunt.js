934fd7866296cde6cc6c727432ddd5764717f56e
ostens <erinjayaustin@gmail.com>
2023-01-03 13:52:23 +0000

    add distance to next checkpoint

---
+++ fe-local-treasures/styles/startHunt.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: "60%",
+  },
+  callout: {
+    padding: 5,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  calloutIcon: {
+    margin: 5,
+  },
+});
+export { styles };

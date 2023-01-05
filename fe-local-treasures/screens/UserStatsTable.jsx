25c8d25217aacd48149b4a15893dbcbfc250eb02
rose-phillips <ms.rosephillips@gmail.com>
2023-01-05 11:59:54 +0000

    styes user stats

--- fe-local-treasures/screens/UserStatsTable.jsx
+++ fe-local-treasures/screens/UserStatsTable.jsx
@@ -5,49 +5,62 @@ import { styles } from "../styles/huntsTable";
 import { globalStyles } from "../styles/globalStyles";
 import { textStyles } from "../styles/textStyles";
 import { fetchStatsByUser } from "../utils/api/huntApi";
+import { formatTime } from "../utils/formatTime";
 
+export const UserStatsTable = () => {
+  const [userStats, setUserStats] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export const UserStatsTable = ()=> {
-    const [userStats, setUserStats] = useState([])
-    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetchStatsByUser("ernie").then((stats) => {
+      setUserStats(stats);
+      setIsLoading(false);
+    });
+  }, []);
 
-useEffect(()=>{
-    fetchStatsByUser('ernie').then((stats)=> {
-        setUserStats(stats)
-        setIsLoading(false)
-    })
-},[])
-
-const stats = () => {
-    return (
-    userStats.map((stat) => {
-    return (
-    <DataTable.Row key={stat.id}>
-          <DataTable.Cell style={styles.widthThree}>
+  const stats = () => {
+    return userStats.map((stat) => {
+      return (
+        <DataTable.Row key={stat.id}>
+          <DataTable.Cell style={styles.widthTwo}>
             <Text style={textStyles.oxygenRegLight14}>{stat.hunt}</Text>
           </DataTable.Cell>
-          <DataTable.Cell style={styles.widthTwo}>
-            <Text style={textStyles.oxygenRegLight14}>{stat.time}</Text>
+          <DataTable.Cell style={styles.widthOneAlignRight}>
+            <Text style={textStyles.oxygenRegLight14}>
+              {" "}
+              {formatTime(stat.time)}
+            </Text>
           </DataTable.Cell>
-          <DataTable.Cell style={styles.widthOne}>
+          <DataTable.Cell style={styles.widthTwoAlignCenter}>
             <Text style={textStyles.oxygenRegLight14}>{stat.date}</Text>
           </DataTable.Cell>
-
-          
-        </DataTable.Row>)
-}))}
-return (
-    <View style={globalStyles.container} >
-    <View><Text>Hello</Text></View>
-    <DataTable>
-        <DataTable.Header>
-            <DataTable.Title color={'white'}>Hunt</DataTable.Title>
-            <DataTable.Title color={'white'}>Time</DataTable.Title>
-            <DataTable.Title color={'white'}>Date</DataTable.Title>
+        </DataTable.Row>
+      );
+    });
+  };
+  return (
+    <View style={globalStyles.container}>
+      <View>
+        <Text>
+          <Text style={textStyles.oxygenBoldLight24}>Your hunt stats </Text>
+          <Text style={textStyles.oxygenRegLight14}>(smashed it...)</Text>
+        </Text>
+      </View>
+      <View height={20}></View>
+      <DataTable>
+        <DataTable.Header style={styles.headerRow}>
+          <DataTable.Title style={styles.widthTwo}>
+            <Text style={textStyles.oxygenBoldLight14}>Hunt</Text>
+          </DataTable.Title>
+          <DataTable.Title style={styles.widthOneAlignRight}>
+            <Text style={textStyles.oxygenBoldLight14}>Time</Text>
+          </DataTable.Title>
+          <DataTable.Title style={styles.widthTwoAlignCenter}>
+            <Text style={textStyles.oxygenBoldLight14}>Date</Text>
+          </DataTable.Title>
         </DataTable.Header>
         {stats()}
-    </DataTable>
+      </DataTable>
     </View>
-)
-
-}
+  );
+};
--- fe-local-treasures/styles/huntsTable.js
+++ fe-local-treasures/styles/huntsTable.js
@@ -6,11 +6,17 @@ const styles = StyleSheet.create({
     margin: 5,
   },
 
+  headerRow: {
+    backgroundColor: "rgba(231,238,245,0.2)",
+  },
+
   widthOne: {
     flex: 1,
     justifyContent: "center",
   },
   widthTwo: { flex: 2 },
+  widthOneAlignRight: { flex: 1, justifyContent: "flex-end" },
+  widthTwoAlignCenter: { flex: 2, justifyContent: "center" },
   widthThree: { flex: 3 },
   arrow: {
     color: "#EAAA00",
